191a5f795220446a14eb3a1e74d06f4d6d624ae1
Marilyn Celis <marilyncelisgutierrez@gmail.com>
2024-08-13 01:07:03 -0300

    modificaciones en puertos

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 export const config = {
     server:{
-        port: process.env.PORT || process.env.SERVER_PORT
+        port: process.env.PORT || 3000
     },
     mongo:{
         url:process.env.MONGO_URL
