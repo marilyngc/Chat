a339ffa5c9dc1db7bb42dbb1e650077243cd5672
Marilyn Celis <marilyncelisgutierrez@gmail.com>
2024-08-13 10:32:52 -0300

    modificación de env para conexión de db con cliente en producción

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/socket.io": {
-        target:"http://localhost:8080",
+        target:process.env.VITE_BACKEND_URL,
         ws:true
       }
     }
